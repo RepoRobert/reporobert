970a30a84c55c904e86992a16cf649a300413da6
Robert <74838686+RepoRobert@users.noreply.github.com>
2023-11-03 19:19:30 +0200

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
-  author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  website: "https://reporobert.vercel.app/", // replace this with your deployed domain
+  author: "Robert",
+  desc: "Robert's dev blog",
+  title: "Robert's Blog",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 10,
